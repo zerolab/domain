f8066ec6fa96315511fb772b44bf109c957cdefe
Dan Braghis <dan.braghis@torchbox.com>
2017-04-13 00:52:12 +0100

    more

--- domain_alias/tests/src/Functional/DomainAliasEnvironmentTest.php
+++ domain_alias/tests/src/Functional/DomainAliasEnvironmentTest.php
@@ -81,19 +81,18 @@ class DomainAliasEnvironmentTest extends DomainTestBase {
     user_role_grant_permissions(RoleInterface::ANONYMOUS_ID, array('administer domains'));
     // For a non-aliased request, the url list should be normal.
     $this->drupalGet($domain->getPath());
-    $i = 0;
     foreach ($domains as $domain) {
-      print_r($domain->getHostname());
-      print_r($i++);
       $this->assertSession()->assertEscaped($domain->getHostname());
       $this->assertSession()->linkByHrefExists($domain->getPath(), 0, 'Link found');
     }
     // For an aliased request, the list should be aliased.
     $url = $domain->getScheme() . $alias->getPattern() . $domain->getPort();
     $this->drupalGet($url);
+    var_dump($matches);
+    var_dump($domain->getScheme());
     foreach ($matches as $match) {
       $this->assertSession()->assertEscaped($match->getPattern());
-      var_dump($domain->getHostname());
+      var_dump($match->getPattern());
       $this->assertSession()->linkByHrefExists($domain->getScheme() . $match->getPattern(), 0, 'Link found');
     }
   }
