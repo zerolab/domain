3989b36a2f90832305867d20a1f150d9fe00d1a4
Dan Braghis <dan.braghis@torchbox.com>
2017-04-14 10:10:23 +0100

    test

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-ls
-
 cat > sites/default/services.yml <<- EOM
 parameters:
-renderer.config:
-    required_cache_contexts: ['languages:language_interface', 'theme', 'user.permissions', 'url.site']
+    renderer.config:
+        required_cache_contexts: ['languages:language_interface', 'theme', 'user.permissions', 'url.site']
 EOM
